890e45c075710ab32933774ba360040afdac3cc5
ArchNapixel <141641546+ArchNapixel@users.noreply.github.com>
2025-11-05 22:09:08 +0800

    Delete ColesEnrollmentSystem directory

--- ColesEnrollmentSystem/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Build and run the application with Maven
-cd /d "%~dp0"
-echo Building and running ColesEnrollmentSystem...
-mvn clean compile exec:java -Dexec.mainClass=com.mycompany.colesenrollmentsystem.ColesEnrollmentSystem
-pause
